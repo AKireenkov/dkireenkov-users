a4624b93ca3a92a9509403ee6e1ff05fa309ff5c
AKireenkov <kireeshka32@gmail.com>
2024-05-25 15:11:26 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  })
 
   app.useGlobalPipes(
       new ValidationPipe({
